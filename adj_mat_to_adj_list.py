90200114252d50375b0ec630177fffb19101614c
erick ntambwa muya <erickntambwamuya@gmail.com>
2025-05-26 12:57:18 +0100

    Update adj_mat_to_adj_list.py

--- adj_mat_to_adj_list.py
+++ adj_mat_to_adj_list.py
@@ -5,12 +5,12 @@ adj_mat = [[0, 1, 0, 1, 0, 0],
              [0, 0, 0, 1, 0, 0],
              [0, 0, 0, 0, 0, 0]]
 
-def mat_to_list(matrix):
+def mat_to_list(adj_mat):
     adj_list = {}
-    for i in range(len(matrix)):
+    for i in range(len(adj_mat)):
         adj_list[i] = []
-        for j in range(len(matrix[i])):
-            if matrix[i][j] == 1:
+        for j in range(len(adj_mat[i])):
+            if adj_mat[i][j] == 1:
                 adj_list[i].append(j)
     return adj_list
 adj_list = mat_to_list(adj_mat)
