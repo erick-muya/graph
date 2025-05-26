e1a4933de6f64b4893408c182fb2ec23657a8251
erick ntambwa muya <erickntambwamuya@gmail.com>
2025-05-26 12:24:08 +0100

    g
    gi

---
+++ adj_mat_to_adj_list.py
@@ -0,0 +1,17 @@
+adj_mat = [[0, 1, 0, 1, 0, 0],
+             [0, 0, 1, 0, 0, 0],
+             [1, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 1, 0],
+             [0, 0, 0, 1, 0, 0],
+             [0, 0, 0, 0, 0, 0]]
+
+def mat_to_list(matrix):
+    adj_list = {}
+    for i in range(len(matrix)):
+        adj_list[i] = []
+        for j in range(len(matrix[i])):
+            if matrix[i][j] == 1:
+                adj_list[i].append(j)
+    return adj_list
+adj_list = mat_to_list(adj_mat)
+print(adj_list)
---
+++ test.py
@@ -0,0 +1,1 @@
+print("hello la famille")
